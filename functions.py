4b65e0b401c7cf073c8f6ff199041a53b8eae8db
Albert Naimovic <naimovic.albert@gmail.com>
2023-10-26 17:54:28 +0300

    upload lesson

--- functions.py
+++ functions.py
@@ -76,31 +76,78 @@
 
 
 
-# Write a function that takes two lists and adds the first element in the first list with the first element in the second list, 
+# 1. Write a function that takes two lists and adds the first element in the first list with the first element in the second list, 
 # the second element in the first list with the second element in the second list, etc, etc. Return True if all element combinations add up to the same number.
 # Otherwise, return False. Example:
 # puzzle_pieces([1, 2, 3, 4], [4, 3, 2, 1]) ➞ True
 
+# from typing import List
 
-from typing import List
-
-def puzzle_pieces(list_a: List[int], list_b: List[int]) -> bool:
-    # cia gal ir nesamone
-    if len(list_a) == len(list_b):
-        list_sum: list = [ list_a[x] + list_b[x] for x in range(len(list_a))]
-    else:
-        return False
+# def puzzle_pieces(list_a: List[int], list_b: List[int]) -> bool:
+#     # cia gal ir nesamone
+#     if len(list_a) == len(list_b):
+#         list_sum: list = [ list_a[x] + list_b[x] for x in range(len(list_a))]
+#     else:
+#         return False
     
-    print(list_sum)
-    counter: int = 0
-    for num in list_sum:
-        if num == list_sum[0]:
-            counter += 1
+#     counter: int = 0
+#     for num in list_sum:
+#         if num == list_sum[0]:
+#             counter += 1
 
-    if len(list_sum) == counter:
-        return True
-    else:
-        return False
+#     if len(list_sum) == counter:
+#         return True
+#     else:
+#         return False
 
-print(puzzle_pieces([1, 2, 3, 4], [4, 3, 2, 1]))
-print(puzzle_pieces([9, 8, 7], [7, 8, 9, 10]))
+# print(puzzle_pieces([1, 2, 3, 4], [4, 3, 2, 1]))
+# print(puzzle_pieces([1, 8, 5, 0, -1, 7], [0, -7, -4, 1, 2, -6]))
+# print(puzzle_pieces([1, 2], [-1, -1]))
+# print(puzzle_pieces([9, 8, 7], [7, 8, 9, 10]))
+
+
+# 2. There's a great war between the even and odd numbers.
+# Many numbers already lost their lives in this war and it's your task to end this.
+# You have to determine which group sums larger: the evens or the odds. The larger group wins.
+# Create a function that takes a list of integers, sums the even and odd numbers separately,
+# then returns the difference between the sums of the even and odd numbers.
+
+# from typing import List
+
+# def war_of_numbers(list_a: List[int]) -> int:
+#     even_list = [x for x in list_a if x % 2 == 0]
+#     odd_list = [x for x in list_a if x % 2 != 0]
+    # if sum(even_list) > sum(odd_list):
+    #     return sum(even_list) - sum(odd_list)
+    # else:
+    #     return sum(odd_list) - sum(even_list)
+
+
+# 4.2
+# war_of_numbers = lambda list_a: sum([x for x in list_a if x % 2 == 0]) - sum([x for x in list_a if x % 2 != 0]) if sum([x for x in list_a if x % 2 == 0]) > sum([x for x in list_a if x % 2 != 0]) else sum([x for x in list_a if x % 2 != 0]) - sum([x for x in list_a if x % 2 == 0])
+
+# print(war_of_numbers([2, 8, 7, 5]))
+# print(war_of_numbers([12, 90, 75]))
+# print(war_of_numbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]))
+
+
+# 3. You are given an input array of bigrams, and an array of words.
+# Write a function that returns True if every single bigram from this array can be found at least once in an array of words.
+
+
+# from typing import List
+
+# def can_find(bi_list: List[str], words_list: List[str]) -> bool:
+#     check_list: list = [bi for bi in bi_list if any(bi in word for word in words_list)]
+#     if check_list == bi_list:
+#         return True
+#     return False
+    
+# 4.3
+# can_find = lambda bi_list, words_list: True if bi_list == [x for x in bi_list if any(x in bi for bi in words_list)] else False
+
+# print(can_find(["at", "be", "th", "au"], ["beautiful", "the", "hat"]))
+# print(can_find(["ay", "be", "ta", "cu"], ["maybe", "beta", "abet", "course"]))
+# print(can_find(["th", "fo", "ma", "or"], ["the", "many", "for", "forest"]))
+# print(can_find(["oo", "mi", "ki", "la"], ["milk", "chocolate", "cooks"]))
+
